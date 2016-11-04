98b34c6c8088753353b8fa85b81e22eedc2a4982
Loc Hoang <l_hoang@utexas.edu>
2016-11-04 14:42:05 -0500

    now uses rng; string gives length 5

--- src/gimme/Gimme.scala
+++ src/gimme/Gimme.scala
@@ -1,19 +1,33 @@
 package gimme
 
+import scala.util.Random
+
 /* The class that holds all of the keywords for Gimme */
 class Gimme {
     /* The keyword GIMME which starts lines in this language */
     object GIMME {
+        val rng = new Random()
+
         def NUMBER = {
-            println(10)
+            val result = rng nextInt 100
+            println(result)
         }
 
         def BOOL = {
-            println(true)
+            val result = rng nextInt 1
+            println(result)
         }
 
         def STRING = {
-            println("test")
+            val char1 = rng nextPrintableChar() toString
+            val char2 = rng nextPrintableChar() toString
+            val char3 = rng nextPrintableChar() toString
+            val char4 = rng nextPrintableChar() toString
+            val char5 = rng nextPrintableChar() toString
+            
+            val finalString = char1 + char2 + char3 + char4 + char5
+
+            println(finalString)
         }
     }
 }
