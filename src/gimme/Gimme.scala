f5a636ddefd2f1816edf0126aea67743af0e6cd8
Loc Hoang <l_hoang@utexas.edu>
2016-11-05 09:50:54 -0500

    refactored program state into its own class

--- src/gimme/Gimme.scala
+++ src/gimme/Gimme.scala
@@ -10,50 +10,14 @@ class Gimme {
   // the random number generator we will use to generate things
   val rng = new Random()
 
-  // holds the currently "given" number/string/bool
-  var currentNumber: Int = -1
-  var currentString: String = ""
-  var currentBool: Boolean = false
-
-  // tell you if the variable currently stored in the "current" vars are 
-  // to be used
-  var numberOn = false
-  var stringOn = false
-  var boolOn = false
-
-  /* Sets the current number and nulls everything else */
-  def setNumber(newNumber: Int) = {
-    currentNumber = newNumber
-
-    numberOn = true
-    stringOn = false
-    boolOn = false
-  }
-
-  /* Sets the current string and nulls everything else */
-  def setString(newString: String) = {
-    currentString = newString
-
-    numberOn = false
-    stringOn = true
-    boolOn = false
-  }
-
-  /* Sets the current bool and nulls everything else */
-  def setBool(newBool: Boolean) = {
-    currentBool = newBool
-
-    numberOn = false
-    stringOn = false
-    boolOn = true
-  }
+  val currentState = new ProgramState
 
   /* The keyword GIMME which starts lines in this language */
   object GIMME {
     /* generates a random number from 0 to 100 inclusive and saves it*/
     def NUMBER = {
         val result = rng nextInt 100
-        setNumber(result)
+        currentState setNumber result
     }
 
     /* gimme a random bool */
@@ -63,9 +27,9 @@ class Gimme {
       // depending on 0 or 1, set true or false
       result match {
         case 0 =>
-          setBool(false)
+          currentState setBool false
         case 1 =>
-          setBool(true)
+          currentState setBool true
         case _ => 
           throw new RuntimeException("GIMME BOOL should only " +
                                      "generate 0 or 1")
@@ -74,28 +38,20 @@ class Gimme {
 
     /* gives me (at the moment) a 5 character string */
     def STRING = {
-      val char1 = (rng nextPrintableChar) toString
-      val char2 = (rng nextPrintableChar) toString
-      val char3 = (rng nextPrintableChar) toString
-      val char4 = (rng nextPrintableChar) toString
-      val char5 = (rng nextPrintableChar) toString
+      val char1 = rng.nextPrintableChar toString
+      val char2 = rng.nextPrintableChar toString
+      val char3 = rng.nextPrintableChar toString
+      val char4 = rng.nextPrintableChar toString
+      val char5 = rng.nextPrintableChar toString
         
       val finalString = char1 + char2 + char3 + char4 + char5
 
-      setString(finalString)
+      currentState setString finalString
     }
 
     /* outputs the last thing that was gimme'd */
     def OUTPUT = {
-      if (!numberOn && !stringOn && !boolOn) {
-        throw new RuntimeException("Trying to output when nothing has been " +
-                                   "gimme'd yet")
-      }
-        
-      if (numberOn) println(currentNumber)
-      else if (stringOn) println(currentString)
-      else if (boolOn) println(currentBool)
-      else throw new RuntimeException("Shouldn't get here if nothing is gimme'd")
+      currentState.output
     }
   }
 }
---
+++ src/gimme/ProgramState.scala
@@ -0,0 +1,51 @@
+package gimme
+
+class ProgramState {
+  // designates type of object the program state is currently holding
+  abstract sealed class CurrentType
+  case object NullType extends CurrentType
+  case object NumType extends CurrentType
+  case object StringType extends CurrentType
+  case object BoolType extends CurrentType
+
+  var currentSetting: CurrentType = NullType
+
+  // holds the currently "given" number/string/bool
+  var currentNumber: Int = -1
+  var currentString: String = ""
+  var currentBool: Boolean = false
+
+  /* Sets the current number */
+  def setNumber(newNumber: Int) = {
+    currentNumber = newNumber
+    currentSetting = NumType
+  }
+
+  /* Sets the current string */
+  def setString(newString: String) = {
+    currentString = newString
+    currentSetting = StringType
+  }
+
+  /* Sets the current bool */
+  def setBool(newBool: Boolean) = {
+    currentBool = newBool
+    currentSetting = BoolType
+  }
+
+  /* outputs the last thing that was gimme'd */
+  def output = {
+    if (currentSetting == NullType) {
+      throw new RuntimeException("Trying to output when nothing has been " +
+                                 "gimme'd yet")
+    }
+        
+    currentSetting match {
+      case NumType => println(currentNumber)
+      case StringType => println(currentString)
+      case BoolType => println(currentBool)
+      case NullType => throw new RuntimeException("Shouldn't get here if nothing is gimme'd")
+    }
+  }
+
+}
