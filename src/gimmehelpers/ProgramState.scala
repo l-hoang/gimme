7d4638316787b849d2368c0cb2918f9dd718eb17
Loc Hoang <l_hoang@utexas.edu>
2016-11-10 11:31:19 -0600

    added binary op functions

--- src/gimmehelpers/ProgramState.scala
+++ src/gimmehelpers/ProgramState.scala
@@ -6,6 +6,10 @@ class ProgramState {
   // program stack
   val gimmeStack = new GimmeStack
 
+  ///////////////////
+  // State setters //
+  ///////////////////
+
   /* Sets the current number */
   def setNumber(newNumber: Int) = {
     gimmeStack addElement newNumber
@@ -21,11 +25,64 @@ class ProgramState {
     gimmeStack addElement newBool
   }
 
-  /* Get the current bool */
+  /* Get the current bool (i.e. the last bool object that was gimme'd */
   def getBool = gimmeStack.getLastBool
+
+  ////////////////
+  // Binary Ops //
+  ////////////////
+
+  /* does stack addition */
+  def stackAddition = {
+    gimmeStack addElement 
+     (gimmeStack.getFirst.asNumber + gimmeStack.getSecond.asNumber)
+  }
+
+  /* does stack addition given an int */
+  def stackAddition(num: Int) = {
+    gimmeStack addElement (num + gimmeStack.getFirst.asNumber)
+  }
+
+  /* does stack subtraction */
+  def stackSubtraction = {
+    gimmeStack addElement 
+     (gimmeStack.getFirst.asNumber - gimmeStack.getSecond.asNumber)
+  }
+
+  /* does stack subtraction given an int */
+  def stackSubtraction(num: Int) = {
+    gimmeStack addElement (num - gimmeStack.getFirst.asNumber)
+  }
+
+  /* does stack multiplication */
+  def stackMultiplication = {
+    gimmeStack addElement 
+     (gimmeStack.getFirst.asNumber * gimmeStack.getSecond.asNumber)
+  }
+
+  /* does stack multiplication given an int */
+  def stackMultiplication(num: Int) = {
+    gimmeStack addElement (num * gimmeStack.getFirst.asNumber)
+  }
+
+  /* does stack division */
+  def stackDivision = {
+    gimmeStack addElement 
+     (gimmeStack.getFirst.asNumber / gimmeStack.getSecond.asNumber)
+  }
+
+  /* does stack division given an int */
+  def stackDivision(num: Int) = {
+    gimmeStack addElement (num / gimmeStack.getFirst.asNumber)
+  }
+
+  ////////////
+  // Output //
+  ////////////
 
   /* outputs the top thing on the stack (i.e. last gimme'd) */
   def output = {
     gimmeStack.outputTop
   }
+
 }
